89238018b37971b43deecc7a0cb2798470b87cd0
Ari Russo <ari.russo@gmail.com>
2017-06-04 19:48:52 -0400

    first pass at chart example

---
+++ examples/chart.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+$:.unshift(File.join("..", "lib"))
+
+# Requires the googlecharts gem, not included in the Gemfile
+require "googlecharts"
+require "pulse-analysis"
+
+# do analysis
+r5 = PulseAnalysis.report("../spec/media/roland_r5_audioout_120bpm_88k.wav")
+
+# chart analysis
+chart = Gchart.new(
+  type: "line",
+  theme: :keynote,
+  title: "Roland R5 Timing",
+  legend: ["Roland R5"],
+  data: [r5.analysis.periods]
+)
+
+# save to file
+chart.file
