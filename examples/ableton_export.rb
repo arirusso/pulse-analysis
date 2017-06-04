b17ab9900148a4e0b163d4cd82a88d0beff94225
Ari Russo <ari.russo@gmail.com>
2017-06-04 18:56:19 -0400

    add examples

--- examples/ableton_export.rb
+++ examples/ableton_export.rb
@@ -3,5 +3,5 @@ $:.unshift(File.join("..", "lib"))
 
 require "pulse-analysis"
 
-report = PulseAnalysis.report("../test/media/ableton_live_exportaudio_120bpm_48k.wav")
+report = PulseAnalysis.report("../spec/media/ableton_live_exportaudio_120bpm_48k.wav")
 puts report.to_s
