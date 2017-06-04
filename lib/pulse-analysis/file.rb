e10de1e290c5bad0ed9e72b74f63ba9a3893288d
Ari Russo <ari.russo@gmail.com>
2017-06-04 18:55:20 -0400

    use ruby file class

--- lib/pulse-analysis/file.rb
+++ lib/pulse-analysis/file.rb
@@ -3,15 +3,22 @@ module PulseAnalysis
   # An audio file
   class File
 
-    attr_reader :num_channels, :path, :sample_rate, :size
+    extend Forwardable
+
+    attr_reader :file, :num_channels, :size
+
+    def_delegators :@file, :path
 
     # @param [::File, String] file_or_path
     def initialize(file_or_path)
-      @path = file_or_path.kind_of?(::File) ? file_or_path.path : file_or_path
-      @file = RubyAudio::Sound.open(@path)
-      @size = ::File.size(@path)
-      @num_channels = @file.info.channels
-      @sample_rate = @file.info.samplerate
+      @file = file_or_path.kind_of?(::File) ? file_or_path : ::File.new(file_or_path)
+      @sound = RubyAudio::Sound.open(@file)
+      @size = ::File.size(@file)
+      @num_channels = @sound.info.channels
+    end
+
+    def sample_rate
+      @sample_rate ||= @sound.info.samplerate
     end
 
     # @param [Hash] options
@@ -19,16 +26,16 @@ module PulseAnalysis
     # @return [Array<Array<Float>>, Array<Float>] File data
     def read(options = {})
       if logger = options[:logger]
-        logger.puts("Reading audio file #{@path}")
+        logger.puts("Reading audio file #{@file}")
       end
       buffer = RubyAudio::Buffer.float(@size, @num_channels)
       begin
-        @file.seek(0)
-        @file.read(buffer, @size)
+        @sound.seek(0)
+        @sound.read(buffer, @size)
         data = buffer.to_a
       rescue RubyAudio::Error
       end
-      logger.puts("Finished reading audio file #{@path}") if logger
+      logger.puts("Finished reading audio file #{@file}") if logger
       data
     end
 
