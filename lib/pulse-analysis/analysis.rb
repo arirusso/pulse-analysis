f68f538425acbfd3abd0d3d31ab2e12a342d72db
Ari Russo <ari.russo@gmail.com>
2017-06-04 18:54:54 -0400

    add analysis results

--- lib/pulse-analysis/analysis.rb
+++ lib/pulse-analysis/analysis.rb
@@ -23,6 +23,86 @@ module PulseAnalysis
       populate_periods
     end
 
+    # Usable length of the audio file in seconds
+    # @return [Integer]
+    def length_in_seconds
+      @length_in_seconds ||= @sound.size / @sound.sample_rate
+    end
+
+    # Usable length of the audio file in the format (MMmSSs)
+    # @return [String]
+    def length_formatted
+      if @length_formatted.nil?
+        min_sec = length_in_seconds.divmod(60)
+        @length_formatted = "#{min_sec[0]}m#{min_sec[1]}s"
+      end
+      @length_formatted
+    end
+
+    # Average number of samples between pulses
+    # @return [Float]
+    def average_period
+      @average_period ||= @periods.inject(&:+).to_f / num_pulses
+    end
+
+    # Number of usable pulses in the audio file
+    # @return [Integer]
+    def num_pulses
+      @num_pulses ||= @periods.count
+    end
+
+    # Longest number of samples between pulse
+    # @return [Integer]
+    def longest_period
+      @longest_period ||= @periods.max
+    end
+
+    # Shortest number of samples between pulse
+    # @return [Integer]
+    def shortest_period
+      @shortest_period ||= @periods.min
+    end
+
+    # Tempo of the audio file in beats per minute (BPM)
+    # Assumes that the pulse is 16th notes as per the Innerclock
+    # Litmus Test
+    # @return [Float]
+    def tempo_bpm
+      if @tempo_bpm.nil?
+        seconds = average_period / @sound.sample_rate
+        division = 4 # 16th notes
+        @tempo_bpm = 60 / seconds / division
+      end
+      @tempo_bpm
+    end
+
+    # Largest sequential abberation between pulses
+    # @return [Integer]
+    def largest_abberation
+      abberations.max
+    end
+
+    # Average sequential abberation between pulses
+    # @return [Float]
+    def average_abberation
+      abberations.inject(&:+).to_f / abberations.count
+    end
+
+    def abberations
+      if @abberations.nil?
+        i = 0
+        abberations = @periods.map do |period|
+          last_period = i >= 0 ? @periods[i - 1] : 0
+          abberation = period - last_period
+          i += 1
+          abberation
+        end
+        abberations.reject!(&:zero?)
+        @abberations = abberations.map(&:abs)
+      end
+      @abberations
+    end
+
     private
 
     def populate_periods
