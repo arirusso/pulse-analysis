0fc1462987922039c86c625a2fff2e83b8a5a3b8
Ari Russo <ari.russo@gmail.com>
2017-06-04 23:34:02 -0400

    update examples

--- examples/ableton_export.rb
+++ examples/ableton_export.rb
@@ -4,4 +4,4 @@ $:.unshift(File.join("..", "lib"))
 require "pulse-analysis"
 
 report = PulseAnalysis.report("../spec/media/ableton_live_exportaudio_120bpm_48k.wav")
-puts report.to_s
+puts report.to_h
--- examples/roland_r5.rb
+++ examples/roland_r5.rb
@@ -4,4 +4,4 @@ $:.unshift(File.join("..", "lib"))
 require "pulse-analysis"
 
 report = PulseAnalysis.report("../spec/media/roland_r5_audioout_120bpm_88k.wav")
-puts report.to_s
+puts report.to_h
