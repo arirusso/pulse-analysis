93635a76e250a53fc589b3c7351cd1b2bd321b65
Ari Russo <ari.russo@gmail.com>
2017-06-06 11:04:30 -0400

    bump gem version

--- lib/pulse-analysis.rb
+++ lib/pulse-analysis.rb
@@ -22,7 +22,7 @@ require "pulse-analysis/sound"
 
 module PulseAnalysis
 
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 
   # Analyze the given audio file with the given options and generate a report
   # @param [::File, String] file_or_path File or path to audio file to run analysis on
